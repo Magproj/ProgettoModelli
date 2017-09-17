40ba142c0e2cb71cc2e0a88fd980952e71f6b60a
msancilio3 <31994278+msancilio3@users.noreply.github.com>
2017-09-17 13:02:43 +0200

    Add files via upload

--- modificaAdattatore.php
+++ modificaAdattatore.php
@@ -17,41 +17,39 @@
 
     //dati del form
     $id=$_POST['identificatore'];
+
     
-    if($id===null || $id<=0){
-    trigger_error('Errore nell\'inserimento del dato. ', E_USER_NOTICE);
+    //database
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'progetto');
+    
+    //get connection
+    $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if($mysqli->connect_errno){
+    	trigger_error('Connection failed: ' . $mysqli->connect_error, E_USER_NOTICE);
     }
 
     
-    //accesso al database
-    $host='localhost';
-    $username='root';
-    $password='';
-    $db_nome='progetto';
-    $result = mysql_pconnect($host, $username, $password);
-    if($result===false){
-        trigger_error('Impossibile connettersi al server: ' . mysql_error(), E_USER_NOTICE);
-    }
-    
-    $result = mysql_select_db($db_nome);
-    if($result===false){
-        trigger_error('Accesso al database non riuscito: ' . mysql_error(), E_USER_NOTICE);
-    }
-    
     //comando SQL
-    $sql = "SELECT * FROM adattatore WHERE Id=$id";
-    $result = mysql_query($sql);
-    $conta= mysql_num_rows($result);
+    $sql = sprintf("SELECT * FROM adattatore WHERE Id='%s'", mysql_escape_string($id));
+    $result = $mysqli->query($sql);
+    $conta= mysqli_num_rows($result);
+    
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
     if($conta===1){
         
+	
+	
 	$str = 'I dati dell\'adattatore cercato sono i seguenti: <br><br>';
         echo $str;
-        
-        $id = mysql_result($result, 0, 'id');
-	$str = 'Identificatore:  ' . $id . ' </br>';
+     
+	$str = 'Identificatore:  ' . $row['id'] . ' </br>';
         echo $str;
-        $stato = mysql_result($result, 0, 'stato');
+        $stato = $row['stato'];
         if($stato===true){
 	    $str = 'Stato: Attivo <a href="modificaStatoAdatt.html">Edit</a></br>';
             echo $str;
@@ -59,7 +57,7 @@
 	    $str = 'Stato: Non attivo <a href="modificaStatoAdatt.html">Edit</a></br>';
             echo $str;
         }
-        $idsensore = mysql_result($result, 0, 'id_sensore');
+        $idsensore = $row['id_sensore'];
 	$str = 'Identificatore sensore: ' . $idsensore . ' <a href="modificaSensoreAdatt.html">Edit</a></br>';
         echo $str;
     
