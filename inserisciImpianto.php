1b6066e421b64f06bd39b65baf46a350ee3c7afc
msancilio3 <31994278+msancilio3@users.noreply.github.com>
2017-09-18 07:46:46 +0200

    Add files via upload

--- inserisciImpianto.php
+++ inserisciImpianto.php
@@ -22,6 +22,7 @@
 	    header('Location:Login.html');
       }
     
+    if($_SESSION['username']==='admin' && $_SESSION['password']==='admin' ){
     
     //dati del form
     $id=htmlentities($_POST['identificatore']);
@@ -45,20 +46,19 @@
 	trigger_error('Errore nell\'inserimento del dato. ', E_USER_NOTICE);
     }
     
-    //accesso al database
-    $host='localhost';
-    $username='root';
-    $password='';
-    $db_nome='progetto';
-    $result = mysql_pconnect($host, $username, $password);
-    if($result===false){
-        trigger_error('Impossibile connettersi al server: ' . mysql_error(), E_USER_NOTICE);
-    }
+    //database
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'progetto');
     
-    $result = mysql_select_db($db_nome);
-    if($result===false){
-        trigger_error('Accesso al database non riuscito: ' . mysql_error(), E_USER_NOTICE);
+    //get connection
+    $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if($mysqli->connect_errno){
+    	trigger_error('Connection failed: ' . $mysqli->connect_error, E_USER_NOTICE);
     }
+
     
     if(isset($_POST['stato'])) { 
     
@@ -66,15 +66,18 @@
     }
     
     //comando SQL
-    $sql = "INSERT INTO impianto (Id, tipo, dimensione, stato, id_cliente) VALUES ('$id',  '$tipo', '$dimensione', '$stato', '$idCliente')";
-    // You may use a 'trasparent' anti-CSRF control, like this:
-	include_once __DIR__ . '/libs/csrf/csrfprotector.php'; // FIXED
-	csrfProtector::init();
-  // Sensitive code follows...
-    if(mysql_query($sql)===true){
+    $sql = sprintf("INSERT INTO impianto (Id, tipo, dimensione, stato, id_cliente) VALUES ('%s',  '%s', '%s', '%s', '%s')", mysqli_real_escape_string($mysqli, $id), mysqli_real_escape_string($mysqli, $tipo), mysqli_real_escape_string($mysqli, $dimensione), mysqli_real_escape_string($mysqli, $stato), mysqli_real_escape_string($mysqli, $idCliente));
+    $result = $mysqli->query($sql);
+    
+    
+    if($result===true){
         echo 'Dati memorizzati correttamente<br />';
     } else {
         echo 'Attenzione, si è verificato un errore: ' . mysql_error();
+    }
+    
+    }else{
+	trigger_error('Non è autorizzato a modificare questi dati. ' . $mysqli->connect_error, E_USER_NOTICE);
     }
 
 ?>
