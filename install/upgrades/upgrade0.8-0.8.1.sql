5a22731a8719e15394b99317c9f6cf58e26a8071
Marta Sancilio <m.sancilio5@studenti.uniba.it>
2017-12-08 12:06:27 +0100

    delete

--- install/upgrades/upgrade0.8-0.8.1.sql
+++
@@ -1,4 +0,0 @@
-# File to upgrade OpenClinic from 0.8 to 0.8.1
-# After use this, you can delete it
-
-UPDATE setting_tbl SET version='0.8.20130912';
--- install/upgrades/upgrade0.8.1-0.8.2.sql
+++
@@ -1,4 +0,0 @@
-# File to upgrade OpenClinic from 0.8.1 to 0.8.2
-# After use this, you can delete it
-
-UPDATE setting_tbl SET version='0.8.20160412';
