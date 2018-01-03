4a079c13339579a1f0e369f7dcd44bad2f398ac4
Marta Sancilio <m.sancilio5@studenti.uniba.it>
2018-01-03 14:47:54 +0100

    third commit

---
+++ openems-master/edge/src/io/openems/core/utilities/api/WritePOJO.java
@@ -0,0 +1,30 @@
+package io.openems.core.utilities.api;
+
+import io.openems.api.channel.WriteChannel;
+import io.openems.api.exception.WriteChannelException;
+/**
+ *
+ * @author FENECON GmbH
+ *
+ */
+public class WritePOJO extends WriteObject {
+	/**
+	 * Constant
+	 */
+	public final Object value;
+
+	public WritePOJO(Object value) {
+		super();
+		this.value = value;
+	}
+
+	@Override
+	public void pushWrite(WriteChannel<?> writeChannel) throws WriteChannelException {
+		writeChannel.pushWriteFromObject(this.value);
+	}
+
+	@Override
+	public String valueToString() {
+		return this.value.toString();
+	}
+}
