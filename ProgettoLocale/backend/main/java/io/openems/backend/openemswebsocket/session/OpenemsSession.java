660d805397c0759ce3dbaad768f164433784efd7
Marta Sancilio <m.sancilio5@studenti.uniba.it>
2018-01-04 20:34:48 +0100

    try fix

---
+++ ProgettoLocale/backend/main/java/io/openems/backend/openemswebsocket/session/OpenemsSession.java
@@ -0,0 +1,19 @@
+package io.openems.backend.openemswebsocket.session;
+
+import io.openems.common.session.Session;
+/**
+ *
+ * @author FENECON GmbH
+ *
+ */
+public class OpenemsSession extends Session<OpenemsSessionData> {
+
+	protected OpenemsSession(String token, OpenemsSessionData data) {
+		super(token, data);
+	}
+
+	@Override
+	public String toString() {
+		return "Device [" + getData().getDevices().getNamesString() + "]";
+	}
+}
---
+++ ProgettoLocale/edge/io/openems/core/BridgeInitializedEventListener.java
@@ -0,0 +1,11 @@
+package io.openems.core;
+/**
+ *
+ * @author FENECON GmbH
+ *
+ */
+public interface BridgeInitializedEventListener {
+
+	void onBridgeInitialized();
+
+}
